e0481c9d54eb9c569e4f5d7d0cfd8a82c4296ee3
WillyVaessen <github@willyvaessen.nl>
2023-11-28 10:43:09 +0100

    Day 7. Struggling bit time with this assignment. I have an idea what to do, but not sure HOW to do it.

--- Day7/Day7_Part1.js
+++ Day7/Day7_Part1.js
@@ -31,15 +31,60 @@ const fs = require('fs');
 // const INPUT = fs.readFileSync('./Day7_Input', 'utf-8').split('\n');
 const INPUT = fs.readFileSync('./Day7_Input_Example', 'utf-8').split('\n');
 const WIRES = new Map();
+const COMMAND_Sequence = new Map();
 const LOGIC_GATES = ["AND", "OR", "LSHIFT", "RSHIFT", "NOT"];
 let signal = 0;
 // console.log(INPUT_XMPL)
+let integerCount = 0;
 
 //  Define BitWise functions
 function bitwiseNOT(value) {
-    console.log("Value entered is: " + value);
+    // console.log("Value entered is: " + value);
     return ~value & 0xFFFF; // Apply bitwise NOT and mask with 0xFFFF
 }
+
+for (let i = 0; i < INPUT.length; i++) {
+    // console.log(INPUT[i]);
+    let outputWire = INPUT[i].split(' -> ')[1];
+    let inputCommandString = INPUT[i].split(' -> ')[0];
+    // console.log(outputWire + " = " + inputCommandString);
+    WIRES.set(outputWire, inputCommandString);
+}
+let runCounter = 1;
+// First run
+console.log("--- Start Run " + runCounter + " -------------------------------------")
+integerCount = 0;
+console.log("Starting run " + runCounter);
+console.log("There are " + WIRES.size + " entries in the map.");
+console.log("There are now " + integerCount + " integers in the map");
+runCounter++;
+let commandToRun;
+for (const [outputWire, inputCommandString] of WIRES.entries()) {
+    // console.log(`${outputWire} = ${inputCommandString}`);
+    // console.log(inputCommandString);
+    if (parseInt(inputCommandString)) {
+        // console.log("Parsed Integer is: " + parseInt(inputCommandString));
+        // WIRES.set(outputWire, parseInt(inputCommandString))
+        signal = parseInt(inputCommandString);
+        COMMAND_Sequence.set(outputWire, signal)
+        integerCount++;
+    } else {
+        console.log(inputCommandString + " is not an Integer, processing command.");
+        // console.log("----------------------------------------")
+        // console.log("Outcome for wire " + outputWire + " is: " + parseCommand(inputCommandString));
+        // console.log("----------------------------------------")
+        // commandToRun = parseCommand(inputCommandString);
+        // WIRES.set(outputWire, commandToRun);
+    }
+}
+
+console.log(integerCount);  // Counts the amount of integers in the WIRES map.
+console.log(WIRES);
+console.log("----------------------------------------")
+console.log(COMMAND_Sequence)
+console.log("----------------------------------------")
+console.log("--- End Run " + runCounter + " ---------------------------------------")
+//  ---------------------------------------------------------------------------------------------------------------- //
 
 
 function parseCommand(inputCommand) {
@@ -49,44 +94,46 @@ function parseCommand(inputCommand) {
     // console.log(result);
     if (match) {
         const operator = match[1];
-        console.log(`Found a match: Operator=${operator}`);
+        // console.log(`Found a match: Operator=${operator}`);
         if (operator === "NOT") {
             return bitwiseNOT(inputCommand.split(' ')[1])
         } else if (operator === "AND") {
-            console.log("AND Operator found");
-            console.log(inputCommand);
-            console.log(inputCommand.length);
+            // console.log("AND Operator found");
+            // console.log(inputCommand);
+            // console.log(inputCommand.length);
             let arg1 = inputCommand.split(' ')[0];
             let arg2 = inputCommand.split(' ')[2];
-            console.log("First operand is '" + arg1 + "' and second operand is '" + arg2 + "'");
-            return arg1 & arg2;
+            console.log("Operand 1 is: " + arg1);
+            console.log("Operand 2 is: " + arg2);
+            // console.log("First operand is '" + WIRES.get(arg1) + "' and second operand is '" + WIRES.get(arg2) + "'");
+            return WIRES.get(arg1) & WIRES.get(arg2);
 
         } else if (operator === "OR") {
+            // console.log("OR Operator found.")
             let arg1 = inputCommand.split(' ')[0];
-            let arg2 = parseInt(inputCommand.split(' ')[2]);
-            console.log("First operand is '" + arg1 + "' and second operand is '" + arg2 + "'");
-            return arg1 << arg2;
+            let arg2 = inputCommand.split(' ')[2];
+            // console.log("First operand is '" + arg1 + "' and second operand is '" + arg2 + "'");
+            return WIRES.get(arg1) | WIRES.get(arg2);
 
         } else if (operator === "LSHIFT") {
             let arg1 = inputCommand.split(' ')[0];
-            let arg2 = parseInt(inputCommand.split(' ')[2]);
-            console.log("First operand is '" + arg1 + "' and second operand is '" + arg2 + "'");
-            return arg1 << arg2;
+            let arg2 = inputCommand.split(' ')[2];
+            // console.log("First operand is '" + arg1 + "' and second operand is '" + arg2 + "'");
+            return WIRES.get(arg1) << WIRES.get(arg2);
 
         } else if (operator === "RSHIFT") {
-            console.log("RSHIFT Operator found");
+            // console.log("RSHIFT Operator found");
             let arg1 = inputCommand.split(' ')[0];
             let arg2 = parseInt(inputCommand.split(' ')[2]);
-            console.log("First operand is '" + arg1 + "' and second operand is '" + arg2 + "'");
-            return arg1 >>> arg2;
+            // console.log("First operand is '" + arg1 + "' and second operand is '" + arg2 + "'");
+            return WIRES.get(arg1) >>> WIRES.get(arg2);
 
         } else {
-            console.log("No match found. Check if it is a number.");
+            // console.log("No match found. Check if it is a number.");
             if (isNaN(parseInt(inputCommand))) {
                 // console.log(inputCommand + "Is a wire");
             } else return parseInt(inputCommand);
         }
-
     }
 }
 
@@ -96,46 +143,4 @@ function parseCommand(inputCommand) {
 //  The output is right of the ->
 //  With that, we can split each line in an output component and an input component.
 //  Each output is a wire.
-
-for (let i = 0; i < INPUT.length; i++) {
-    const command = (INPUT[i]).trim();
-    console.log("The command is: " + command);
-    const outputWire = command.split(' -> ')[1];
-    const inputCommand = command.split(' -> ')[0];
-    console.log("The input Command is: " + inputCommand);
-    console.log("The output is wire " + outputWire);
-    WIRES.set(outputWire, inputCommand);
-}
-
-console.log(WIRES);
-console.log(WIRES.get('x'));
-let x = parseInt(WIRES.get('x'))
-console.log(x);
-
-console.log(parseCommand(WIRES.get('d')));
-
-//
-// // Iterate through the wires object
-// for (const wire in wires) {
-//     if (wires.hasOwnProperty(wire)) {
-//         const inputCommand = wires[wire];
-//         // console.log(`${wire} is driven by ${inputCommand}`);
-//         // console.log(wire + " = " + inputCommand);
-//         // You can perform additional operations based on each wire and its inputCommand
-//         wires[wire] = parseCommand(inputCommand);
-//     }
-//
-// }
-//
-// // console.log(wires);
-//
-// let command;
-// for (command in wires) {
-//     // console.log(line);
-//     // console.log(wires[line])
-//     console.log("----------------------------------------");
-//     command = wires[command];
-//     console.log(command)
-// }
-
 
