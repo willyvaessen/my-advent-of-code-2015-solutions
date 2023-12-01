5e7c64622dc2dd958766f8f55959b80ffb3191c9
WillyVaessen <github@willyvaessen.nl>
2023-12-01 15:50:11 +0100

    Parking 2015 for now, as I'm attempting the currect AoC edition.

--- Day8/Day8_Part1.js
+++ Day8/Day8_Part1.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
-const INPUT = fs.readFileSync('./Day8_Input', 'utf-8').split('\n');
-// const INPUT = fs.readFileSync('./Day8_Input_Example', 'utf-8').split('\n');
+// const INPUT = fs.readFileSync('./Day8_Input', 'utf-8').split('\n');
+const INPUT = fs.readFileSync('./Day8_Input_Example', 'utf-8').split('\n');
 console.log(INPUT);
+
+console.log(INPUT[0].length);
+
